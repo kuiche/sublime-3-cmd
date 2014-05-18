95eded29f0002865869866352e1c7597c505947f
kuiche <samtkeates@gmail.com>
2014-05-19 00:18:33 +0100

    stubs for other os

--- cmd_launcher.py
+++ cmd_launcher.py
@@ -1,12 +1,22 @@
 import sublime, sublime_plugin, subprocess, os
 
 class CmdCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        subprocess.Popen("cmd")
+	def run(self, edit):
+		if os.name == 'nt': # windows
+			subprocess.Popen("cmd")
+		elif os.name == 'mac' : # mac
+			pass
+		elif os.name == 'posix': #linux
+			pass
 
 class CmdHereCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        fname = self.view.file_name()
-        path = os.sep.join(fname.split(os.sep)[:-1])
-        subprocess.Popen(["cmd", "/K", "cd", "/d", path])
+	def run(self, edit):
+		fname = self.view.file_name()
+		path = os.sep.join(fname.split(os.sep)[:-1])
+		if os.name == 'nt': # windows
+			subprocess.Popen(["cmd", "/K", "cd", "/d", path])
+		elif os.name == 'mac': # mac
+			pass
+		elif os.name == 'posix': # linux
+			pass
 
